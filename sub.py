a44bafb54898b7e7cdc208b4646e1da208585afc
jahenvins <61344081+jahenvins@users.noreply.github.com>
2021-10-01 11:28:03 +0530

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,7 @@
+#stare the input numbers
+num1=input("Enter the first number: ");
+num2=input("Enter the second number: ");
+#Subtract two numbers
+sub=float(num2)-float(num1);
+#display the substract
+print("The subtract of {0} and {1} is {2}".format(num2,num1,sub) )
